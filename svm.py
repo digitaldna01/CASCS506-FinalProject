eae6e6c42ccc9c1013b4e67179a449a90c239427
Jae Hong Lee <ll.leejaehong.ll@gmail.com>
2024-12-09 20:50:15 -0500

    finished SVM

--- svm.py
+++ svm.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 class SVM:
-    def __init__(self, learning_rate=0.001, lambda_param=0.01, n_iters=1000):
+    def __init__(self, learning_rate=0.001, C=1.0, n_iters=1000):
         self.lr = learning_rate
-        self.lambda_param = lambda_param  # 규제 parameters
+        self.C = C  # 규제 parameters
         self.n_iters = n_iters
         self.w = None  # weights Initiate Weights to zero
         self.b = 0     # bias Initiate bias to 0
@@ -14,22 +14,34 @@ class SVM:
 
         # transfork: check y is {1, -1} shape
         y_ = np.where(y <= 0, -1, 1) # Based on Y value, if it is less than 0 change it to -1, so there are only two y values, -1 and 1 values.
-
+        
         # Gradient Descent optimization
+        # prev_loss = float('inf')
         for _ in range(self.n_iters): # Run each iterations. 
             for idx, x_i in enumerate(X): # Iterate each rows of Train Data
                 condition = y_[idx] * (np.dot(x_i, self.w) + self.b) >= 1
                 if condition:
                     # Loss function Gradient (힌지 손실 X)
-                    self.w -= self.lr * (2 * self.lambda_param * self.w)
+                    self.w -= self.lr * (2 * self.w)
                 else:
                     # 힌지 손실 포함
-                    self.w -= self.lr * (2 * self.lambda_param * self.w - np.dot(x_i, y_[idx]))
-                    self.b -= self.lr * y_[idx]
+                    self.w -= self.lr * (2 * self.w - self.C *  np.dot(x_i, y_[idx]))
+                    self.b -= self.lr * self.C * y_[idx]
+            
+            # current_loss = self._compute_loss(X, y_)
+            # if abs(prev_loss - current_loss) < 1e-5:  # 변화가 작으면 중단
+            #     break
+            # prev_loss = current_loss
+        
+        
 
     def predict(self, X):
         approx = np.dot(X, self.w) + self.b
         return np.sign(approx)  # class {1, -1} return
+    
+    def _compute_loss(self, X, y_):
+        hinge_loss = np.maximum(0, 1 - y_ * (np.dot(X, self.w) + self.b))
+        return 0.5 * np.dot(self.w, self.w) + self.C * np.sum(hinge_loss)
 
 # Data Excample
 if __name__ == "__main__":
